ad4f0473954f53b91406f5689bdb8a12d3bf1ecc
maysara-elshewehy <maysara.elshewehy@gmail.com>
2025-06-28 18:35:10 +0300

    Dummy PR

--- lib/string/utils/codepoint/codepoint.test.zig
+++ lib/string/utils/codepoint/codepoint.test.zig
@@ -10,14 +10,59 @@
 
 // ╔══════════════════════════════════════ PACK ══════════════════════════════════════╗
 
-
+    const Codepoint = @import("./codepoint.zig");
+    const testing   = @import("std").testing;
 
 // ╚══════════════════════════════════════════════════════════════════════════════════╝
 
 
 
+// ╔══════════════════════════════════════ INIT ══════════════════════════════════════╗
+
+    const SAMPLE = "A©€😀";
+    const RESULT = [_]struct { utf8: []const u8, cp: u21 } {
+        .{ .utf8 = "A",  .cp = 0x00041 },
+        .{ .utf8 = "©",  .cp = 0x000A9 },
+        .{ .utf8 = "€",  .cp = 0x020AC },
+        .{ .utf8 = "😀", .cp = 0x1F600 },
+    };
+
+// ╚══════════════════════════════════════════════════════════════════════════════════╝
+
+
 // ╔══════════════════════════════════════ TEST ══════════════════════════════════════╗
 
+    test "peek & next" {
+        var iterator = Codepoint.Utf8Iterator { .src = SAMPLE };
 
+        for(0..RESULT.len) |i| {
+
+            // codepoint
+            if(iterator.nextCodepoint()) |cp| {
+                try testing.expectEqual(RESULT[i].utf8.len, cp.len);
+                try testing.expectEqual(RESULT[i].cp, cp.src);
+
+                // reset
+                iterator.pos -= cp.len;
+            } else return error.NextCodepointError;
+
+            // utf8 slice
+            if(iterator.nextSlice()) |slice| {
+                try testing.expectEqual(RESULT[i].utf8.len, slice.len);
+                try testing.expectEqualStrings(RESULT[i].utf8, slice);
+
+                // reset
+                iterator.pos -= slice.len;
+            } else return error.NextSliceError;
+
+            // length
+            if(iterator.nextLength()) |len| {
+                try testing.expectEqual(RESULT[i].utf8.len, len);
+            } else return error.NextLengthError;
+        }
+
+        // position
+        try testing.expectEqual(SAMPLE.len, iterator.pos);
+    }
 
 // ╚══════════════════════════════════════════════════════════════════════════════════╝
--- lib/string/utils/codepoint/codepoint.zig
+++ lib/string/utils/codepoint/codepoint.zig
@@ -10,7 +10,8 @@
 
 // ╔══════════════════════════════════════ PACK ══════════════════════════════════════╗
 
-
+    const utf8 = @import("../utf8/utf8.zig");
+    const Codepoint = @This();
 
 // ╚══════════════════════════════════════════════════════════════════════════════════╝
 
@@ -18,6 +19,93 @@
 
 // ╔══════════════════════════════════════ CORE ══════════════════════════════════════╗
 
+    // ┌─────────────────────────── Fields ───────────────────────────┐
 
+        /// Numeric value of the Unicode codepoint (U+0000 to U+10FFFF).
+        src: u21 = 0,
+
+        /// Length of this codepoint in UTF-8 (1-4 bytes).
+        len: u3  = 0,
+
+    // └──────────────────────────────────────────────────────────────┘
+
+
+    // ┌────────────────────────── Methods ───────────────────────────┐
+
+
+
+    // └──────────────────────────────────────────────────────────────┘
+
+// ╚══════════════════════════════════════════════════════════════════════════════════╝
+
+
+
+// ╔══════════════════════════════════════ ITER ══════════════════════════════════════╗
+
+    pub const Utf8Iterator = struct {
+
+        // ┌─────────────────────────── Fields ───────────────────────────┐
+
+            /// The UTF-8 encoded string that the iterator will traverse.
+            src: []const u8,
+
+            /// The current byte position in the string.
+            pos: usize = 0,
+
+        // └──────────────────────────────────────────────────────────────┘
+
+
+        // ┌────────────────────────── Methods ───────────────────────────┐
+
+            /// Decodes and returns the next codepoint and increments the position.
+            pub inline fn nextCodepoint(self: *Utf8Iterator) ?Codepoint {
+                if(self.peekCodepoint()) |cp| {
+                    self.pos += cp.len;
+                    return cp;
+                } else return null;
+            }
+
+            /// Returns the next utf8 slice and increments the position.
+            pub inline fn nextSlice(self: *Utf8Iterator) ?[]const u8 {
+                if(self.peekSlice()) |slice| {
+                    self.pos += slice.len;
+                    return slice;
+                } else return null;
+            }
+
+            /// Returns the next codepoint length and increments the position.
+            pub inline fn nextLength(self: *Utf8Iterator) ?u3 {
+                if(self.peekLength()) |len| {
+                    self.pos += len;
+                    return len;
+                } else return null;
+            }
+
+            /// Decodes and returns the next codepoint without incrementing the position.
+            pub inline fn peekCodepoint(self: Utf8Iterator) ?Codepoint {
+                if(self.peekLength()) |len| {
+                    return Codepoint {
+                        .src = utf8.decode(self.src[self.pos..self.pos+len]),
+                        .len = len
+                    };
+                } else return null;
+            }
+
+            /// Returns the next utf8 slice without incrementing the position.
+            pub inline fn peekSlice(self: Utf8Iterator) ?[]const u8 {
+                if(self.peekLength()) |len| {
+                    return self.src[self.pos..self.pos+len];
+                } else return null;
+            }
+
+            /// Returns the next codepoint length without incrementing the position.
+            pub inline fn peekLength(self: Utf8Iterator) ?u3 {
+                if(self.pos == self.src.len) return null;
+                const len = utf8.getSequenceLength(self.src[self.pos]);
+                return if(len == 0) null else len;
+            }
+
+        // └──────────────────────────────────────────────────────────────┘
+    };
 
 // ╚══════════════════════════════════════════════════════════════════════════════════╝
